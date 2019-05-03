eade5cc702cfc5a2d4c55228bad0575bd6a405e5
jon <me@caesirekin.me>
2019-05-03 14:57:10 +0800

    Update

--- src/hello-world.c
+++ src/hello-world.c
@@ -3,15 +3,15 @@
 int main (int argc, char const* argv[]) {
     printf("This is a type test writen by Jon\n");
     printf("sizeof(char): %lu\n", sizeof(char));
-    // printf("sizeof(bool): %lu\n", sizeof(bool));
+    // printf("sizeof(bool): %lu\n", sizeof(bool)); // C doesn't have bool ?
     printf("sizeof(short): %lu\n", sizeof(short));
     printf("sizeof(int): %lu\n", sizeof(int));
     printf("sizeof(long): %lu\n", sizeof(long));
     printf("sizeof(float): %lu\n", sizeof(float));
     printf("sizeof(double): %lu\n", sizeof(double));
-
+    printf("\n");
     printf("sizeof(char*): %lu\n", sizeof(char*));
-    // printf("sizeof(bool*): %lu\n", sizeof(bool*));
+    // printf("sizeof(bool*): %lu\n", sizeof(bool*)); // C doesn't have bool ?
     printf("sizeof(short*): %lu\n", sizeof(short*));
     printf("sizeof(int*): %lu\n", sizeof(int*));
     printf("sizeof(long*): %lu\n", sizeof(long*));
